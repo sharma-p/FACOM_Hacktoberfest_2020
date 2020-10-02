0f462a019cd0363effda2edbe8dff8927a39751a
KushagraArora300 <kushagraarora300@gmail.com>
2020-10-02 23:52:43 +0530

    Array Input Function in java

---
+++ Java/ArrayInputFunction.java
@@ -0,0 +1,33 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ArrayInputFunction {
+
+	public static void printArray(int[] arr) {
+	   int n = arr.length;
+		for(int i=0;i<n;i++) {
+	    	   System.out.println(arr[i]);
+	}
+	}	
+	public static int[] takeInput() {
+	    	Scanner s = new Scanner (System.in);
+	       int n = s.nextInt();
+	       int[] arr = new int[n];
+	       
+	       for(int i=0;i<n;i++) {
+	    	   System.out.println("Enter element at " + i + "th index");
+	    	   arr[i] = s.nextInt(); 
+	       }
+	       return arr;
+	}
+	public static void main(String[] args) {
+		
+		int[] arr = takeInput();
+		 printArray(arr);
+	       
+	       }
+
+	
+
+}
